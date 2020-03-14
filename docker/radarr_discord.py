c17a64785cafaf5fb55cb91b871ae9e99e9a8677
samwiseg0 <samwiseg0@gmail.com>
2020-03-14 19:30:19 -0400

    Add packages so the script can be ran inside docker

---
+++ docker/radarr_discord.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+import os
+import logging
+import sys
+import json
+import datetime
+import re
+import requests
+import script_config
+
+discord_headers = {'content-type': 'application/json'}
+
+# Set up the log file
+log_filename = os.path.join(os.path.dirname(sys.argv[0]), 'radarr_notification.log')
+logging.basicConfig(
+    filename=log_filename,
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s - %(message)s'
+)
+log = logging.getLogger('Radarr')
+
+def get_imdb_rating(imdb_id):
+    get_imdb = requests.get('https://imdb-api.com/API/Ratings/{}/{}'.format(script_config.radarr_imdbapi_key, imdb_id)).json()
+    imdb_rating = get_imdb['imDb']
+    if not imdb_rating:
+            imdb_rating = "Unknown"
+    return imdb_rating
+
+def utc_now_iso():
+    utcnow = datetime.datetime.utcnow()
+    return utcnow.isoformat()
+
+# Get Event Type
+eventtype = os.environ.get('radarr_eventtype')
+
+if eventtype == 'test':
+    TEST_MODE = True
+else:
+    TEST_MODE = False
+
+#Get ENV variables
+movie_id = os.environ.get('radarr_movie_id')
+if not movie_id:
+    TEST_MODE = True
+
+media_title = os.environ.get('radarr_movie_title')
+if not media_title:
+    media_title = 'The Lego Movie'
+
+imdb_id = os.environ.get('radarr_movie_imdbid')
+if not imdb_id:
+    imdb_id = 'tt1490017'
+
+quality = os.environ.get('radarr_moviefile_quality')
+if not quality:
+    quality = 'Bluray-2160p'
+
+scene_name = os.environ.get('radarr_moviefile_scenename')
+
+imdb_rating = get_imdb_rating(imdb_id)
+
+imdb_url = 'https://www.imdb.com/title/' + imdb_id
+
+#Get Radarr data
+radarr_api_url = '{}api/movie/{}?apikey={}'.format(script_config.radarr_url, movie_id, script_config.radarr_key)
+
+radarr = requests.get(radarr_api_url)
+
+radarr_data = radarr.json()
+
+if not TEST_MODE:
+    year = radarr_data['year']
+
+if TEST_MODE:
+    scene_name = 'THIS IS A TEST MESSAGE'
+
+    year = '2014'
+
+#Get Trailer Link from Radarr
+try:
+    trailer_link = 'https://www.youtube.com/watch?v={}'.format(radarr_data['youTubeTrailerId'])
+except:
+    trailer_link = 'None'
+
+title_slug = re.sub(r'[?|$|.|!|:]', r'', media_title).replace(' ', '-')
+
+#Get data from TMDB
+moviedb_api_url = 'https://api.themoviedb.org/3/find/{}?api_key={}&external_source=imdb_id'.format(imdb_id, script_config.moviedb_key)
+
+moviedb_api = requests.get(moviedb_api_url)
+
+moviedb_api_data = moviedb_api.json()
+
+radarr_id = moviedb_api_data['movie_results'][0]['id']
+
+try:
+    overview = moviedb_api_data['movie_results'][0]['overview']
+except:
+    overview = 'None'
+
+try:
+    release = moviedb_api_data['movie_results'][0]['release_date']
+except:
+    release = 'None'
+
+#Get Poster from TMDB
+poster_path = moviedb_api_data['movie_results'][0]['poster_path']
+
+try:
+    poster_path = 'https://image.tmdb.org/t/p/w185' + poster_path
+except TypeError:
+    #Send a generic poster if there is not one for this movie
+    poster_path = 'https://i.imgur.com/GoqfZJe.jpg'
+
+message = {
+    'username': script_config.radarr_discord_user,
+    'content': 'New movie downloaded - {} ({}) IMDb: {}'.format(media_title, year, imdb_rating),
+    'embeds': [
+        {
+            'author': {
+                'name': 'Movies',
+                'url': script_config.radarr_url,
+                'icon_url': script_config.radarr_icon
+            },
+            'title': '{} ({})'.format(media_title, year),
+            'color': 3394662,
+            'url': '{}movie/{}-{}'.format(script_config.radarr_url, title_slug.lower(), radarr_id),
+            'image': {
+                'url': poster_path
+            },
+            'fields': [
+                {
+                    'name': 'Quality',
+                    'value': quality,
+                    'inline': True
+                },
+                {
+                    'name': 'Release Date',
+                    'value': release,
+                    'inline': True
+                }
+            ]
+        },
+        {
+            'title': 'Overview',
+            'color': 3381708,
+            'description': overview
+        },
+        {
+            'title': 'Trailer',
+            'color': 3394662,
+            'description': trailer_link,
+        },
+        {
+            'title': 'IMDb URL',
+            'color': 13421619,
+            'description': imdb_url,
+            'footer': {
+                'text': '{}'.format(scene_name)
+            },
+            'timestamp': utc_now_iso()
+        }
+
+    ]
+}
+
+# Log json
+log.info(json.dumps(message, sort_keys=True, indent=4, separators=(',', ': ')))
+
+# Send notification
+sender = requests.post(script_config.radarr_discord_url, headers=discord_headers, json=message)
+
+# Log response from discord
+log.info(sender.content)
---
+++ docker/sonarr_discord.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+import os
+import logging
+import sys
+import json
+import datetime
+import requests
+import script_config
+
+discord_headers = {'content-type': 'application/json'}
+
+# Set up the log file
+log_filename = os.path.join(os.path.dirname(sys.argv[0]), 'sonarr_notification.log')
+logging.basicConfig(
+    filename=log_filename,
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s - %(message)s')
+log = logging.getLogger('Sonarr')
+
+def utc_now_iso():
+    utcnow = datetime.datetime.utcnow()
+    return utcnow.isoformat()
+
+def main():
+    # Get/set ENV variables
+    eventtype = os.environ.get('sonarr_eventtype')
+
+    season = os.environ.get('sonarr_episodefile_seasonnumber')
+
+    episode = os.environ.get('sonarr_episodefile_episodenumbers')
+
+    tvdb_id = os.environ.get('sonarr_series_tvdbid')
+
+    scene_name = os.environ.get('sonarr_episodefile_scenename')
+
+    media_title = os.environ.get('sonarr_series_title')
+
+    episode_title = os.environ.get('sonarr_episodefile_episodetitles')
+
+    quality = os.environ.get('sonarr_episodefile_quality')
+
+    is_upgrade = os.environ.get('sonarr_isupgrade')
+
+    overview = ''
+
+    if eventtype == 'Test':
+        log.info('Sonarr script test succeeded.')
+        sys.exit(0)
+
+    # Get show information from skyhook
+    get_skyhook = requests.get(script_config.skyhook_url + str(tvdb_id))
+
+    skyhook_data = get_skyhook.json()
+
+    title_slug = skyhook_data['slug']
+
+    # Get banner image for show
+    try:
+        banner = skyhook_data['seasons'][int(season)]['images'][1]['url']
+    except Exception as ex:
+        log.error(ex)
+        banner = skyhook_data['images'][1]['url']
+
+    for line in skyhook_data['episodes']:
+        try:
+            if int(line['seasonNumber']) == int(season) and \
+                    int(line['episodeNumber']) == int(episode):
+
+                if line['overview']:
+                    overview = line['overview']
+
+        except Exception as ex:
+            log.error(ex)
+
+
+    if not overview:
+        overview = 'None'
+
+    if len(str(season)) == 1:
+        season = '0{}'.format(season)
+
+    if len(str(episode)) == 1:
+        episode = '0{}'.format(episode)
+
+    if is_upgrade == 'True':
+        content = 'Upgraded Episode - {}: {}'.format(media_title, episode_title)
+        is_upgrade = 'Yes!'
+
+    else:
+        content = 'New episode downloaded - {}: {}'.format(media_title, episode_title)
+        is_upgrade = 'Nope'
+
+
+    message = {
+        "username": script_config.sonarr_discord_user,
+        "content": content,
+        "embeds": [
+            {
+                "author": {
+                    "name": "TV",
+                    "url": script_config.sonarr_url,
+                    "icon_url": script_config.sonarr_icon
+                    },
+                "title": "{}: {}".format(media_title, episode_title),
+                "color": 3394662,
+                "url": "{}series/{}".format(script_config.sonarr_url, title_slug),
+                "image": {
+                    "url": banner
+                    },
+                "fields": [
+                    {
+                        "name": "Episode",
+                        "value": "s{}e{}".format(season, episode),
+                        "inline": True
+                    },
+                    {
+                        "name": "Quality",
+                        "value": quality,
+                        "inline": True
+                    },
+                    {
+                        "name": "Upgrade?",
+                        "value": is_upgrade,
+                        "inline": True
+                    }
+                    ]
+            },
+            {
+                "title": "Overview",
+                "color": 3381708,
+                "description": overview,
+                "footer": {
+                    "text": "{}".format(scene_name)
+                    },
+                "timestamp": utc_now_iso()
+            }
+
+        ]
+    }
+
+    log.info(json.dumps(message, sort_keys=True, indent=4, separators=(',', ': ')))
+
+    # Send notification
+    sender = requests.post(script_config.sonarr_discord_url, headers=discord_headers, json=message)
+
+    # Log response
+    log.info(sender.content)
+
+# Call main
+main()
